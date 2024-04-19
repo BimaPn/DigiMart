c2164be6ad56fef40c5c32df821fab4e7e073148
lonewolf <bimaptr12@gmail.com>
2024-04-19 22:16:09 +0700

    Redesign services label in homepage.

--- src/assets/services.ts
+++ src/assets/services.ts
@@ -4,21 +4,25 @@ const services = [
         icon: './icons/shipping.svg',
         title: 'Free Shipping',
         description: 'Free shipping on order',
+        background: "bg-red-50"
     },
     {
         icon: './icons/clock.svg',
         title: 'Online Support 24/7',
         description: 'Support online 24 hours a day',
+        background: "bg-green-50"
     },
     {
         icon: './icons/discount.svg',
         title: 'Daily Offers',
         description: 'Discount up to 70% OFF',
+        background: "bg-yellow-50"
     },
     {
         icon: './icons/secure.svg',
         title: 'Secure Payment',
         description: 'Gurantee secure payments',
+        background: "bg-blue-50"
     },
 ]
 
--- src/components/Home/OurServices.tsx
+++ src/components/Home/OurServices.tsx
@@ -4,15 +4,15 @@ import Image from "next/image"
 const OurServices = () => {
     return(
     <BoxSection className="mt-10 xs:mt-12 sm:mt-14 md:mt-20 mb-10 sm:mb-16">
-        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-3 xs:gap-4 md:gap-8'>
+        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4'>
             {services.map((item,i)=> (
-                <div key={i} className='basis-1/2 sm:basis-1/4 py-2 flex items-start justify-center gap-2 xs:gap-4 sm:gap-3'>
-                    <div className="w-10 xs:w-16 sm:w-14">
+                <div key={i} className={`basis-1/2 sm:basis-1/4 flex flex-col gap-1 ${item.background} bg-green-50 p-3 xs:p-4 rounded-2xl`}>
+                    <div className="w-10">
                         <Image src={item.icon} className="w-full" width={100} height={100} alt={item.title} />
                     </div>
-                    <div className='basis-[70%] xs:basis-[60%]'>
-                        <h1 className='font-bold text-xs xs:text-base sm:text-sm md:text-lg'>{item.title}</h1>
-                        <span className="text-[10px] xs:text-base sm:text-xs md:text-base">{item.description}</span>
+                    <div className='flex flex-col gap-1'>
+                        <h1 className='font-bold text-sm xs:text-base sm:text-sm md:text-lg'>{item.title}</h1>
+                        <span className="text-xs xs:text-base sm:text-xs md:text-base leading-normal inline-block">{item.description}</span>
                     </div>
                 </div>
             ))}
